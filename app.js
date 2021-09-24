d70820b79061ee5679df7b8989be72ab19784155
Taylor Bish <tbish1121@gmail.com>
2021-09-24 17:04:36 -0400

    Added the grid container with all buttons as well as added functionality.

---
+++ app.js
@@ -0,0 +1,41 @@
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+const clear = document.querySelector('.clear');
+
+function clearDisplay() {
+    display.innerHTML = '';
+}
+
+function add(i) {
+    return function() {
+        if(buttons[i].innerHTML == '*') {
+            display.innerHTML += '*';
+        } else if (buttons[i].innerHTML == '+') {
+            display.innerHTML += '+';
+        } else if(buttons[i].innerHTML == '/') {
+            display.innerHTML += '/';
+        } else if(buttons[i].innerHTML == '-') {
+        display.innerHTML += '-'; 
+        } else {
+            display.innerHTML += buttons[i].innerHTML;
+            console.log('num')
+        }
+    } 
+}
+
+function calc() {
+    return function() {
+        display.innerHTML = eval(display.innerHTML);
+    }
+    
+}
+
+for(let i = 0; i < buttons.length; i++) {
+    if(buttons[i].innerHTML == '=') {
+        buttons[i].addEventListener('click', calc());
+    } else {
+        buttons[i].addEventListener('click', add(i))
+    }
+}
+
+clear.addEventListener('click', clearDisplay)
